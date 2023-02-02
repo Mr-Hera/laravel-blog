56d5cb7aa76e6c874e631e5c311389fa56248a89
Mr-Hera <randellhera@gmail.com>
2023-02-02 16:44:13 +0300

    Feat: added search bar + components & reseeded db

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function index() {
-        return view('blogs.index');
+        return view('blogs.index', [
+            'blogs' => Blog::latest()->paginate(3)
+        ]);
     }
 }
---
+++ resources/views/partials/_search.blade.php
@@ -0,0 +1,14 @@
+<form action="/">
+    <div class="relative border-2 border-gray-100 mx-10 mt-8 mb-10">
+      <div class="absolute top-4 left-3">
+        <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+      </div>
+      <input type="text" name="search" class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+        placeholder="Search Bmw Blogs..." />
+      <div class="absolute top-2 right-2">
+        <button type="submit" class="h-10 w-20 text-white bg-black hover:bg-bmw">
+          Search
+        </button>
+      </div>
+    </div>
+</form>
